02834f661603941b13dba61bf63e97ab9217101d
LiorGurevich <lior.gure@gmal.com>
2024-12-12 19:00:33 +0200

    FINISH: Ex4 SafeAndSound part1

--- DebugWorkshop/SafeAndSound/part1.cpp
+++ DebugWorkshop/SafeAndSound/part1.cpp
@@ -1,8 +1,12 @@
 #include "part1.h"
 #include <iostream>
 
+// needed to increase the size of dest by one, but not in the func
+#define DESTSIZE 13
+
 char* string_copy(char* dest, unsigned int destsize, char* src)
 {
+	destsize--;
 	char* ret = dest;
 	while (*dest++ = *src++)
 		;
@@ -12,10 +16,10 @@ char* string_copy(char* dest, unsigned int destsize, char* src)
 void part1()
 {
 	char password[] = "secret";
-	char dest[12];
+	char dest[DESTSIZE];
 	char src[] = "hello world!";
 
-	string_copy(dest, 12, src);
+	string_copy(dest, DESTSIZE, src);
 
 	std::cout << src << std::endl;
 	std::cout << dest << std::endl;
